be7866a4cb1f06de4cee0002c933f54f28e9b907
Rodrigo Sakae <devsakae@gmail.com>
2022-09-29 15:46:06 -0300

    Requisito 1 com CSS básico, redux configurado e muito feliz de ter conquistado os 3 pontos agora é rumo aos 80% pro time não ser rebaixado

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+export const LOGA_USUARIO = 'LOGA_USUARIO';
+
+const appLogin = (dados) => ({
+  type: LOGA_USUARIO,
+  payload: dados,
+});
+
+export { appLogin };
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,25 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGA_USUARIO } from '../actions';
+
+const estadoInicial = {
+  user: {
+    email: '',
+    password: '',
+  },
+};
+
+const userReducer = (state = estadoInicial, action) => {
+  switch (action.type) {
+  case LOGA_USUARIO:
+    return {
+      ...state,
+      user: {
+        ...action.payload,
+      },
+    };
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
