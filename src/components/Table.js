fd40cab58a22d9253844568918ce1888b1c22bdd
Rodrigo Sakae <devsakae@gmail.com>
2022-10-03 16:42:05 -0300

    Testes passaram de 60% de cobertura

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import teste from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <div>Table</div>
+      <table className="expenses" hidden={ expenses.length < 1 }>
+        <tr className="expenses-header">
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+        { expenses.map((expense) => {
+          const rate = expense.exchangeRates[expense.currency].ask;
+          return (
+            <tr key={ expense.id }>
+              <td>{ expense.description }</td>
+              <td>{ expense.tag }</td>
+              <td>{ expense.method }</td>
+              <td>{ expense.value }</td>
+              <td>{ expense.exchangeRates[expense.currency].name }</td>
+              <td>{ Number(rate).toFixed(2) }</td>
+              <td>
+                { (rate * expense.value).toFixed(2) }
+              </td>
+              <td>Real</td>
+              <td>Editar/Excluir</td>
+            </tr>
+          );
+        }) }
+      </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: teste.shape({}),
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  ...state.wallet,
+});
+
+export default connect(mapStateToProps)(Table);
--- src/tests/03.Wallet.test.js
+++ src/tests/03.Wallet.test.js
@@ -1,29 +1,42 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 import Wallet from '../pages/Wallet';
+import mockData from './helpers/mockData';
 
 describe('Testa efetivamente a carteira', () => {
   test('Verifica se existem os campos necessários', () => {
     renderWithRouterAndRedux(<Wallet />);
     const valueInput = screen.getByTestId('value-input');
     const descriptionInput = screen.getByTestId('description-input');
-    const currencyInput = screen.getByTestId('currency-input');
     const methodInput = screen.getByTestId('method-input');
     const tagInput = screen.getByTestId('tag-input');
     expect(valueInput).toBeInTheDocument();
     expect(valueInput).toHaveTextContent('');
     expect(descriptionInput).toBeInTheDocument();
     expect(descriptionInput).toHaveTextContent('');
-    expect(currencyInput).toBeInTheDocument();
     expect(methodInput).toBeInTheDocument();
     expect(tagInput).toBeInTheDocument();
   });
-  // test('Insere valores e testa botões', () => {
-  //   renderWithRouterAndRedux(<Wallet />);
-  //   const valueInput = screen.getByTestId('value-input');
-  //   const descriptionInput = screen.getByTestId('description-input');
-  //   userEvent.type(valueInput)
-  // });
+  test('Insere valores e testa botões', () => {
+    renderWithRouterAndRedux(<Wallet />);
+    const valueInput = screen.getByTestId('value-input');
+    const descriptionInput = screen.getByTestId('description-input');
+    const botaoNovaDespesa = screen.getByRole('button');
+    userEvent.type(valueInput, '10');
+    userEvent.type(descriptionInput, 'Dez dolares');
+    userEvent.click(botaoNovaDespesa);
+    expect(valueInput.textContent).toBe('');
+    expect(descriptionInput.textContent).toBe('');
+  });
+  test('Verifica se as moedas são renderizadas', async () => {
+    jest.spyOn(global, 'fetch').mockImplementation(async () => ({ json: async () => mockData }));
+    renderWithRouterAndRedux(<Wallet />);
+    const currencies = screen.getByTestId('currency-input');
+    expect(currencies).toBeInTheDocument();
+    await waitFor(() => expect(currencies).toHaveValue('USD'));
+    userEvent.selectOptions(currencies, 'CAD');
+    expect(fetch).toHaveBeenCalled();
+  });
 });
