fdeab0f12813d5ff50c05bd81d666f674995fc57
Rodrigo Sakae <devsakae@gmail.com>
2022-10-04 08:27:34 -0300

    [Bug fixes] Requisitos 7 e 8 não estavam passando, verifiquei o Cypress com :open e descobri os erros (inclusão de tbody e toFixed(2) no expenses.value)

--- src/components/Table.js
+++ src/components/Table.js
@@ -15,44 +15,48 @@ class Table extends Component {
     const { expenses } = this.props;
     return (
       <table className="expenses" hidden={ expenses.length < 1 }>
-        <tr className="expenses-header">
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
-        { expenses.map((expense) => {
-          const rate = expense.exchangeRates[expense.currency].ask;
-          return (
-            <tr key={ expense.id }>
-              <td>{ expense.description }</td>
-              <td>{ expense.tag }</td>
-              <td>{ expense.method }</td>
-              <td>{ expense.value }</td>
-              <td>{ expense.exchangeRates[expense.currency].name }</td>
-              <td>{ Number(rate).toFixed(2) }</td>
-              <td>
-                { (rate * expense.value).toFixed(2) }
-              </td>
-              <td>Real</td>
-              <td>
-                <button
-                  type="button"
-                  data-testid="delete-btn"
-                  id={ expense.id }
-                  onClick={ (e) => this.deletaDespesa(e) }
-                >
-                  Excluir
-                </button>
-              </td>
-            </tr>
-          );
-        }) }
+        <thead>
+          <tr className="expenses-header">
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          { expenses.map((expense) => {
+            const rate = expense.exchangeRates[expense.currency].ask;
+            return (
+              <tr key={ expense.id }>
+                <td>{ expense.description }</td>
+                <td>{ expense.tag }</td>
+                <td>{ expense.method }</td>
+                <td>{ Number(expense.value).toFixed(2) }</td>
+                <td>{ expense.exchangeRates[expense.currency].name }</td>
+                <td>{ Number(rate).toFixed(2) }</td>
+                <td>
+                  { (rate * expense.value).toFixed(2) }
+                </td>
+                <td>Real</td>
+                <td>
+                  <button
+                    type="button"
+                    data-testid="delete-btn"
+                    id={ expense.id }
+                    onClick={ (e) => this.deletaDespesa(e) }
+                  >
+                    Excluir
+                  </button>
+                </td>
+              </tr>
+            );
+          }) }
+        </tbody>
       </table>
     );
   }
