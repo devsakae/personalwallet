6005581d99f8fdf5ad1edfb4c2151bd6dd2aacd6
Rodrigo Sakae <devsakae@gmail.com>
2022-10-06 07:51:42 -0300

    Teste não atinge o catch error

--- src/tests/04.Table.test.js
+++ src/tests/04.Table.test.js
@@ -5,14 +5,18 @@ import { renderWithRouterAndRedux } from './helpers/renderWith';
 import Wallet from '../pages/Wallet';
 import mockData from './helpers/mockData';
 
+// const mockError = { error: { message: 'Erro simulado' } };
+const VALUE_INPUT = 'value-input';
+const DESCRIPTION_INPUT = 'description-input';
+
 describe('Verifica se a despesa foi adicionada corretamente', () => {
   test('É uma tabela?', async () => {
     jest.spyOn(global, 'fetch').mockImplementation(async () => ({ json: async () => mockData }));
     renderWithRouterAndRedux(<Wallet />);
     const currencies = screen.getByTestId('currency-input');
     await waitFor(() => userEvent.selectOptions(currencies, 'USD'));
-    userEvent.type(screen.getByTestId('value-input'), '10');
-    userEvent.type(screen.getByTestId('description-input'), 'Dez dolares');
+    userEvent.type(screen.getByTestId(VALUE_INPUT), '10');
+    userEvent.type(screen.getByTestId(DESCRIPTION_INPUT), 'Dez dolares');
     userEvent.click(screen.getByRole('button'));
     await waitFor(() => {
       const tabelaExiste = screen.getByRole('table');
@@ -34,11 +38,16 @@ describe('Verifica se a despesa foi adicionada corretamente', () => {
     const valorConvertido = screen.getByText(/Valor convertido/i);
     expect(valorConvertido).toBeInTheDocument();
     await waitFor(() => userEvent.click(screen.getByTestId('edit-btn')));
-    userEvent.type(screen.getByTestId('value-input'), '20');
-    userEvent.type(screen.getByTestId('description-input'), 'Vinte');
+    userEvent.type(screen.getByTestId(VALUE_INPUT), '20');
+    userEvent.type(screen.getByTestId(DESCRIPTION_INPUT), 'Vinte');
     const doisBotaoDeEditar = await screen.getAllByRole('button', { name: /editar despesa/i });
     userEvent.click(doisBotaoDeEditar[0]);
     expect(screen.getByText(/Vinte/i)).toBeInTheDocument();
     userEvent.click(screen.getByTestId('delete-btn'));
   });
+  // test('Simular erro no fetch e verifica se é pego no catch', async () => {
+  //   jest.spyOn(global, 'fetch').mockImplementation(async () => ({ json: async () => mockError }));
+  //   renderWithRouterAndRedux(<Wallet />);
+  //   expect(fetch).toThrow();
+  // });
 });
